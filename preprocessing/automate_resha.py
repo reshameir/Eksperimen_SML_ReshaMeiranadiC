d56c11dfb279dbc788ae2f2d2893d06848540276
reshameir <reshameiranadi@gmail.com>
2025-07-17 10:54:17 +0700

    first commit

---
+++ preprocessing/automate_resha.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
